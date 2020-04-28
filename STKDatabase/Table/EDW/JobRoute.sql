efbb2ee4772bf872d57370e020b359adcd889e86
Jeff Phillips <jeff.phillips@orchid-ortho.com>
2020-04-28 18:45:50 -0400

    Added JobRoute and WorkStation tables

---
+++ STKDatabase/Table/EDW/JobRoute.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [EDW].[JobRoute]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [JobId] INT NULL,
+    [OperationNo] DECIMAL(15, 6) NULL,
+    [WorkCenterId] INT NULL,
+    [ProductTimePerUnit] DECIMAL(15, 6) NULL, 
+    [SetupTimeHours] DECIMAL(15, 6) NULL, 
+    CONSTRAINT [FK_JobRoute_ToJob] FOREIGN KEY ([JobId]) REFERENCES [EDW].[Job]([Id])
+)
---
+++ STKDatabase/Table/EDW/WorkStation.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [EDW].[WorkStation]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [WorkstationCode] NVARCHAR(50) NULL, 
+    [WorkstationDesc] NVARCHAR(500) NULL, 
+    [DepartmentId] INT NULL, 
+    [WorkCenterId] INT NULL, 
+    CONSTRAINT [FK_EDW.[WorkStation_ToWorkCenter] FOREIGN KEY ([WorkCenterId]) REFERENCES [EDW].[WorkCenter]([Id]), 
+    CONSTRAINT [FK_EDW.[WorkStation_ToDepartment] FOREIGN KEY ([DepartmentId]) REFERENCES [EDW].[Department]([Id])
+)
