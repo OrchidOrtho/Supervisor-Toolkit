f6e262edcfd50d450a6c81b51c9a6656c87490f3
Jeff Phillips <jeff.phillips@orchid-ortho.com>
2021-01-22 06:48:39 -0500

    Working on Hr by Hr

--- STKDatabase/Table/HxH/HourbyHourEntryReason.sql
+++ STKDatabase/Table/HxH/HourbyHourEntryReason.sql
@@ -4,6 +4,7 @@
     [HourbyHourEntryId] BIGINT NULL, 
     [ReasonCodeId] INT NULL, 
     [Comments] NVARCHAR(500) NULL, 
+    [ExtraComments] VARCHAR(255) NULL, 
     CONSTRAINT [FK_HourbyHourEntryReason_ToHourbyHourEntry] FOREIGN KEY ([HourbyHourEntryId]) REFERENCES [HXH].[HourbyHourEntry]([Id]), 
     CONSTRAINT [FK_HourbyHourEntryReason_ToReasonCode] FOREIGN KEY ([ReasonCodeId]) REFERENCES [EDW].[ReasonCode]([Id])
 )
---
+++ STKDatabase/Table/HxH/JobFulfillment.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[JobFulfillment]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[EntityName] [nvarchar](100) NULL,
+	[OperationNo] [nvarchar](100) NULL,
+	[JobId] [bigint] NULL,
+	[JobQuantity] [decimal](18, 0) NULL,
+	[StartDate] [date] NULL,
+	[EndDate] [date] NULL,
+	[Fulfilled] [bit] NULL,
+	[QuantityLeft] [decimal](18, 0) NULL,
+	[StartHour] [decimal](18, 0) NULL,
+)
